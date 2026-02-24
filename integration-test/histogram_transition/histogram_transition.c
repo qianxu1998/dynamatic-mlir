04ecd654c7ec67c7331d23f9eeffa1d3e0c88ade
Jiantao Liu <jianliu@ethz.ch>
2026-02-24 16:39:59 +0100

    [SA] Add one more test case for the transitioning situation

---
+++ integration-test/histogram_transition/histogram_transition.c
@@ -0,0 +1,38 @@
+//===- histogram.c ---------------------------------------------*- C -*-===//
+
+#include "histogram_transition.h"
+#include "dynamatic/Integration.h"
+#include <stdlib.h>
+
+void histogram_transition(in_int_t feature[1000], inout_int_t hist[1000],
+                          in_int_t n) {
+  for (int i = 0; i < n; ++i) {
+
+    if (feature[i] > 5) {
+      hist[i] = hist[0] * hist[i];
+    }
+
+    hist[i] = hist[0] + 1;
+  }
+}
+
+int main(void) {
+  in_int_t feature[1000];
+  inout_int_t hist[1000];
+  in_int_t n;
+
+  n = 1000;
+  for (int i = 0; i < 1000; ++i) {
+
+    if (i < 500) {
+      feature[i] = 4;
+    } else {
+      feature[i] = 6;
+    }
+
+    hist[i] = rand() % 100;
+  }
+
+  CALL_KERNEL(histogram_transition, feature, hist, n);
+  return 0;
+}
---
+++ integration-test/histogram_transition/histogram_transition.h
@@ -0,0 +1,10 @@
+#ifndef HISTOGRAM_HISTOGRAM_TRANSITION_H
+#define HISTOGRAM_HISTOGRAM_TRANSITION_H
+
+typedef int in_int_t;
+typedef int inout_int_t;
+
+void histogram_transition(in_int_t feature[1000], inout_int_t hist[1000],
+                          in_int_t n);
+
+#endif // HISTOGRAM_HISTOGRAM_H
